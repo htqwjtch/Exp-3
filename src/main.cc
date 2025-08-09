166699a4a7136d842e5b4f01e999dc6580cbd0fe
htqwjtch <htqwjtch@gmail.com>
2025-08-09 19:57:39 +0300

    add main copy(Expr2, last commit)

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,11 @@
-#include <iostream>
+#include <QApplication>
 
-int main(){
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include "modules/mainModule/mainmodule.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainModule mainModule;
+    mainModule.show();
+    return a.exec();
 }
